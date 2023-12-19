301e8a1fb96f3c188a049a16e76a7ef2378959bd
AndrewGalkin123 <127788127788a@gmail.com>
2023-12-19 12:31:22 +0200

    #

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -2,7 +2,7 @@ import { Breadcrumb } from "antd";
 import { ItemType } from "antd/es/breadcrumb/Breadcrumb";
 
 interface NavigationProps {
-    items: ItemType[]; // Замените "string" на фактический тип элементов в массиве
+    items: ItemType[]; 
   }
 
 const Navigation: React.FC<NavigationProps> = ({ items }) => {
