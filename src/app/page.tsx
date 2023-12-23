7783cec629afcfc2e41f7b0803a00431979deaed
AndrewGalkin123 <127788127788a@gmail.com>
2023-12-23 13:21:12 +0200

    some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import { Calendar, Tooltip, Modal } from "antd";
+import { Calendar, Tooltip } from "antd";
 import { Dayjs } from "dayjs";
 
 interface NoteCalendarProps {}
